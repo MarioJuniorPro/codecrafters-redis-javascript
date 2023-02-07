32d52a219b5cef003eed4a03b8f1474a57e3909d
Mario Costa <mario.junior.pro@gmail.com>
2023-02-07 11:42:36 -0300

    feat: suport ping/pong

--- app/main.js
+++ app/main.js
@@ -6,6 +6,15 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on("data", (data) => {
+    const lines = data.toString().split("\r\n");
+
+    for (const line of lines) {
+        if(line.trim().toUpperCase() === "PING") {
+            connection.write("+PONG\r\n");
+        }
+    }
+  })
 });
 
 server.listen(6379, "127.0.0.1");
